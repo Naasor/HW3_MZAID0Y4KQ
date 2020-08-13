f727041b6a5e00afff2204fa9165b707175e63e5
Naasor <amofasandra7@gmail.com>
2020-08-13 17:14:29 +0000

    GTL Third assignment

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./../css/components.css";
+import "./../css/global.css";
+import "./../css/layout.css";
+import "./../css/sample-wallpaper.css";
+
+
+export class Navbar extends Component {
+  render() {
+    return (
+        <div>
+
+            {/* Navbar */}
+            <nav className="navbar">
+                <div className="container">
+                    <a href="#">Hulk</a>
+
+                    {/* Currently Active */}
+                    <a href="#" className="logo is-active">
+                        Mavel
+                    </a>
+                    <a href="#">Venom</a>
+                </div>
+            </nav>
+        </div>
+    );
+  }
+}
+
+export default Navbar;
